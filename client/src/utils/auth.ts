3c881a0e42d841b5951852073fdb193c4a54215b
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-17 10:20:44 -0600

    finish auth.ts within the client-side directory and create middleware for authenticating the API routes

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,47 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
+
 
 class AuthService {
+  // Returns the decoded JWT token
   getProfile() {
-    // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
+  // Creates a new token variable that gets the JWT token if there is one, returns a boolean for whether or not the token is expired or active
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
+  // Checks if the token given is expired or not using jwtDecode
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
   }
 
+  // Gets the token saved in local storage and returns it
   getToken(): string {
-    // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
+  // Sets the token in local storage and reassigns the users page to the "Board" page
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
+  // Removes the token from local storage and returns the user to the "Board" page
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,6 +5,25 @@ interface JwtPayload {
   username: string;
 }
 
+// Function used to authenticate the token for the API routes
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  // Checks if authHeader exists and uses JWT to verify the token while using the env JWT_SECRET_KEY variable within its algorithm
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
