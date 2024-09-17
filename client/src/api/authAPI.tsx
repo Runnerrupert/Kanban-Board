e52cc18236651312d54d79bf5b741c1a0fd3b5a1
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-17 10:06:52 -0600

    Create the login functions for both authAPI.tsx and auth-routes.ts

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,30 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+// Try/Catch block to gather whether the user can be logged in (if the username and password are correct)
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    });
+
+    // Throws an error if something goes wrong
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+
+    // Variable to hold the gathered JSON information from the fetch request
+    const data = await response.json();
+    return data;
+    
+  } catch (err) {
+    console.log('Error from user login: ', err);
+    return Promise.reject('Could not fetch user info');
+  }
 }
-
-
 
 export { login };
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,13 +3,42 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+// This function takes in the username & password given by the client and checks if that users information is within the database
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  // Information given within the post request
+  const { username, password } = req.body;
+
+  // Searches the DB using the User Model and findOne method from Sequelize to check if the username is within the database
+  const user = await User.findOne({
+    where: { username },
+  });
+
+  // Returns an error message if the username is incorrect
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication Failed' });
+  }
+
+  // Variable that uses bcrypt to hash the password and check if the password the User gave is the same as the password within the database that has been hashed
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+
+  // Returns an error message if the password is incorrect
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication Failed' });
+  }
+
+  // gets the JWT secret key to be used by jwt
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  // Creates a jwt token
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+  // Returns the jwt token
+  return res.json({ token })
 };
 
 const router = Router();
 
-// POST /login - Login a user
+// Creates a post request to login the user, so long as it is Authenticated correctly
 router.post('/login', login);
 
 export default router;
